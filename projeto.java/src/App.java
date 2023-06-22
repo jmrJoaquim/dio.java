1082ef621aac9cc391bb18eafef9f21f832249cf
jmrJoaquim <jmr.joaquimmenezes@gmail.com>
2023-06-22 12:21:58 -0300

    projeto.java

--- projeto.java/src/App.java
+++ projeto.java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Foco, hábito, sucesso!");
+        System.out.println("Foco, hábito, sucesso e dinheiro!");
     }
 }
